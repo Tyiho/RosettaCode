eea23edf6c364979e91a5b7cab9cd8e8865c099d
tyiho <trystan.yt.1@gmail.com>
2026-02-07 22:58:57 -0800

    fix incorrect comment

--- RosettaCodeTest/Problems/100Doors/Trystan/100DoorsTrystan_Test.cs
+++ RosettaCodeTest/Problems/100Doors/Trystan/100DoorsTrystan_Test.cs
@@ -20,7 +20,7 @@ public class _100DoorsTrystan_Test
 
         //only doors that remain open are those that are toggled an odd number of times,
         //which is only the case for perfect squares
-        expected[0] = true; //door 1 is toggled by all 100 passes, so it remains open
+        expected[0] = true; //door 1 is toggled by pass 1, so it remains open
         expected[3] = true; //door 4 is toggled by passes 1,2,4, so it remains open
         expected[8] = true; //door 9 is toggled by passes 1,3,9, so it remains open
         expected[15] = true; //door 16 is toggled by passes 1,2,4,8,16, so it remains open
