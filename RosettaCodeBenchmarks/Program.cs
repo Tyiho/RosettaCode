18c5545fefae2e0521a008cc24fb4cc2bca111c2
MerlSchmits <merl.schmits@gmail.com>
2026-02-16 19:49:10 -0800

    [100Prisoners] Initial probalem solutions.

--- RosettaCodeBenchmarks/Program.cs
+++ RosettaCodeBenchmarks/Program.cs
@@ -1,10 +1,12 @@
 ﻿using BenchmarkDotNet.Running;
 using RosettaCode.Problems._100Doors.Merl.Benchmarks;
+using RosettaCode.Problems._100Prisoners.Merl;
 
 internal class Program
 {
 	private static void Main(string[] args)
 	{
-		BenchmarkRunner.Run<Solve100DoorsBenchmarkTests>();
+		//BenchmarkRunner.Run<Solve100DoorsBenchmarkTests>();
+		BenchmarkRunner.Run<Solve100PrisonersBenchmarkTests>();
 	}
 }
